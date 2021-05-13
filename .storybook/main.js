5238e7f5e2825b0d2aae8198cfdc3303c46978a4
Bouzaiene Chamesddine <chamesddine.bouzaiene@ett.tn>
2021-05-13 03:42:39 +0100

    🎉 initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: [
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../stories/**/*.stories.mdx",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
